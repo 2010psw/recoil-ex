9a958ad4de6fc6204e9e00b56a8c154cfb401378
psw <2010psw@naver.com>
2022-11-01 20:08:34 +0900

    6.8 Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,19 +1,49 @@
+import { faExchange } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
-
+  const { register, watch, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("firstName")} placeholder="First Name" />
-        <input {...register("lastName")} placeholder="Last Name" />
-        <input {...register("username")} placeholder="Username" />
-        <input {...register("password")} placeholder="Password" />
-        <input {...register("password1")} placeholder="Password1" />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          style={{ display: "flex", flexDirection: "column" }}
+          {...register("email", { required: true })}
+          placeholder="Email"
+        />
+        <input
+          {...register("firstName", {
+            required: "firstName required",
+            minLength: 10,
+          })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "lastname need more than 5 letter",
+            },
+          })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", { required: true, minLength: 5 })}
+          placeholder="Password1"
+        />
 
         <button>Add</button>
       </form>
