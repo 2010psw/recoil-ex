fb8afed14a979c43f688eb155079710b9ca125c5
psw <2010psw@naver.com>
2022-11-03 15:37:42 +0900

    7.7 Performance

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DraggableCard from "./Components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -31,13 +32,6 @@ const Board = styled.div`
   border-radius: 5px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  padding: 5px 10px;
-  margin-bottom: 5px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
@@ -57,17 +51,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} key={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.dragHandleProps}
-                        {...magic.draggableProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
+                  <DraggableCard key={toDo} index={index} toDo={toDo} />
                 ))}
                 {magic.placeholder}
               </Board>
